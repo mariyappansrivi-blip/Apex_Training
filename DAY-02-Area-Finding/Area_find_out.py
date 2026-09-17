7487a0ca423b9104a3db5a7472e3603b6de368b1
M.Sakthi <mariyappansrivi@gmail.com>
2026-09-17 10:12:18 +0530

    Add files via upload

---
+++ DAY-02-Area-Finding/Area_find_out.py
@@ -0,0 +1,15 @@
+# Area of Circle
+r = 5
+circle_area = int(3.14 * r * r)
+print("Area of Circle =", circle_area)
+
+# Area of Rectangle
+l = 10
+b = 5
+rectangle_area = l * b
+print("Area of Rectangle =", rectangle_area)
+
+# Area of Square
+s = 4
+square_area = s * s
+print("Area of Square =", square_area)
