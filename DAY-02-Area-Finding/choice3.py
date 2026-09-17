5741aca3a67eaab02f139f88bf142a98dfa06586
M.Sakthi <mariyappansrivi@gmail.com>
2026-09-17 10:27:20 +0530

    Add files via upload

---
+++ DAY-02-Area-Finding/choice3.py
@@ -0,0 +1,21 @@
+choice = 3
+
+while choice <= 3:
+
+    if choice == 1:
+        r = 5
+        area = int(3.14 * r * r)
+        print("Area of Circle =", area)
+
+    elif choice == 2:
+        l = 10
+        b = 5
+        area = l * b
+        print("Area of Rectangle =", area)
+
+    elif choice == 3:
+        s = 4
+        area = s * s
+        print("Area of Square =", area)
+
+    break
