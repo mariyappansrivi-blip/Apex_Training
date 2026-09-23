85eb91a7c27094c766a6d3e91941beccb1be840b
M.Sakthi <mariyappansrivi@gmail.com>
2026-09-23 12:18:03 +0530

    Add files via upload

---
+++ DAY3-OPERATOR_ASSSIGNMENT/identity.py
@@ -0,0 +1,21 @@
+x=["apple","banana"]
+y=["apple","banana"]
+z=x
+
+print(x is z)
+#returns True because zis the same object as x
+
+print (x is y)
+#returns False because x is not the same object as y,even if they have the same content
+
+x=["apple","banana"]
+z=x
+
+print(x is not z)
+#returns False because z is the same object as x
+
+
+output:
+True
+False
+False
